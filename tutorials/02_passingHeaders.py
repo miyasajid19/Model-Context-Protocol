ab8190200dfc779e22b706d71d89863f41f82d5a
miyasajid19 <miyasajid19@gmail.com>
2026-08-19 18:11:09 +0530

    passing header

--- tutorials/02_passingHeaders.py
+++ tutorials/02_passingHeaders.py
@@ -1,19 +1,42 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.agents import create_agent
+import asyncio
+from dotenv import load_dotenv
+import os
+from langchain_openai import ChatOpenAI
+from rich import print
 
-client = MultiServerMCPClient(
-    {
-        "weather": {
-            "transport": "http",
-            "url": "http://localhost:8000/mcp",
-            "headers": {
-                "Authorization": "Bearer YOUR_TOKEN",
-                "X-Custom-Header": "custom-value"
-            },
+load_dotenv()
+
+
+async def main():
+    client = MultiServerMCPClient(
+        {
+            "weather": {
+                "transport": "http",
+                "url": "http://localhost:8000/mcp",
+                "headers": {
+                    "Authorization": "Bearer YOUR_TOKEN",
+                    "X-Custom-Header": "custom-value",
+                },
+            }
         }
-    }
-)
-tools =  client.get_tools()
-agent = create_agent("openai:gpt-5.5", tools)
-response = agent.ainvoke({"messages": "what is the weather in nyc?"})
-print(response)
+    )
+    tools = await client.get_tools()
+    print(tools)
+    agent = create_agent(
+        ChatOpenAI(
+            model_name="MiniMax-M3",
+            api_key=os.getenv("MINIMAX_API_KEY"),
+            base_url=os.getenv("MINIMAX_BASE_URL"),
+        ),
+        tools,
+    )
+    response = await agent.ainvoke(
+        {"messages": [{"role": "user", "content": "what is the weather in nyc?"}]}
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
